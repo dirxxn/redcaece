e5dbd318b0a058d143d1c69b6b853f7839e9540d
Ezequiel Fridman <ezequielfridman@gmail.com>
2015-11-14 21:19:04 -0300

    creo cosas de red

--- src/Entities/Network/Network.java
+++ src/Entities/Network/Network.java
@@ -1,7 +1,11 @@
 package Entities.Network;
 
+import Entities.Equipment.Equipment;
+
 /**
  * Created by efridman on 14/11/15.
  */
 public class Network {
+    private IPAddressV4 ip;
+    private Equipment equipment;
 }
