cf5e95f86b82ee8a3eb426de1064d24a5834405c
jsalinas <jmsalinas88@gmail.com>
2015-11-25 20:31:51 -0300

    Creo clase OperatingSystem

---
+++ src/Entities/Equipment/OperatingSystem.java
@@ -0,0 +1,15 @@
+package Entities.Equipment;
+
+public abstract class OperatingSystem {
+
+	protected String name;
+	protected String version;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
