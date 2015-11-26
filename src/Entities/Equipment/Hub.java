9876c71a60ff153cf23decd63f1ed08bba6fcf8e
DanielaTh <danielar.thea@gmail.com>
2015-11-25 22:32:56 -0300

    Propiedades a Hub y Terminal

--- src/Entities/Equipment/Hub.java
+++ src/Entities/Equipment/Hub.java
@@ -1,7 +1,17 @@
 package Entities.Equipment;
 
+import java.util.ArrayList;
+
+import Entities.Packages.Packet;
+
 /**
  * Created by efridman on 14/11/15.
  */
 public class Hub {
+
+	private int connectionsNumber;
+	private ArrayList<Equipment> equipments = new ArrayList<>();
+
+	public void reenviar(Packet pckt) {
+	}
 }
--- src/Entities/Equipment/Terminal.java
+++ src/Entities/Equipment/Terminal.java
@@ -1,13 +1,17 @@
 package Entities.Equipment;
 
 import java.util.ArrayList;
+import Entities.Osystem.TerminalOs;
+import Entities.Network.IPAddressV4;
 
 /**
  * Created by efridman on 14/11/15.
  */
 public class Terminal {
-	
+
 	private ArrayList<Equipment> equipments = new ArrayList<>();
-	
+	private TerminalOs operatingSystem;
+	private IPAddressV4 associatedIp;
+	private IPAddressV4 ed;
 
 }
