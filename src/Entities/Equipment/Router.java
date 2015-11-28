5fe9afc7ba06c478c635d5dc5137a6b70ce57af3
jsalinas <jmsalinas88@gmail.com>
2015-11-28 11:58:48 -0300

    Cambios en receive e implementacion send

--- src/Entities/Equipment/Router.java
+++ src/Entities/Equipment/Router.java
@@ -21,8 +21,6 @@ public class Router extends NetEquipment {
 	private NavigableMap<Integer, IPAddressV4> routingTable = new TreeMap<>();
 	private Equipment defaultEquipment; // boca por defecto?
 	private NetworkOs operatingSystem;
-	private int maxConnections;
-	private ArrayList<Equipment> connectedEquipments;
 
 	public Router(){
 
@@ -37,46 +35,23 @@ public class Router extends NetEquipment {
 		this.routingTable.put(port, newIp);
 	}
 	
-	public void receivePaket(RoutePacket packet){
-		/*TODO 
-		 * Cuando recibe un paquete de tipo Ruteo:
-			1. Toma la dirección de red de destino del paquete que tiene dentro.
-			2. Luego verifica si la dirección de red destino está en la misma red de algunas de las direcciones
-			de red asociadas. En tal caso envía el paquete al equipo asociado.
-			3. En caso que no encuentre la dirección de red destino del paquete en la lista entonces envía un
-			paquete Ruteo al equipo asociado a la boca por defecto.
-			4. Envía un paquete Sendmsg al remitente informando que no se puede entregar el paquete.
-		 * 
-		 * */
-	}
-	
-	public void receivePaket(Who packet){
-		/*TODO 
-		 * Cuando recibe un paquete tipo WHO:
-			1. Verifica que la dirección de destino se alguna de las propias en tal caso
-			2. Envía un paquete Sendmsg al remitente con toda la información de su sistema operativo y las
-				rutas que tiene.
-			3. En caso contrario no hace nada.
-		 * 
-		 * */
-	}
-
 	@Override
-	public void sendPacket(Packet packet) {
-		// TODO Auto-generated method stub
+	public void sendPacket(Packet<PacketType> packet) {
+		
+		for(Equipment equipment : this.equipments){
+			if(equipment.associatedIp.sameNetwork(packet.getDestination())){
+				equipment.receivePacket(packet);
+				return;
+			}
+		}
 		
 	}
 
 	@Override
-	public void receivePacket(Packet packet) {
+	public void receivePacket(Packet<PacketType> packet) {
 		if(packet instanceof RoutePacket){
-			if(connectedEquipments != null && connectedEquipments.size() > 0){
-				for(Equipment equipment : connectedEquipments){
-					if(equipment.associatedIp.sameNetwork(packet.getDestination())){
-						equipment.receivePacket(packet);
-						return;
-					}
-				}
+			if(this.equipments != null && !this.equipments.isEmpty()){
+				this.sendPacket(packet);
 			}
 			defaultEquipment.receivePacket(packet);
 		}else if(packet.getServiceType() instanceof Who){
@@ -84,13 +59,8 @@ public class Router extends NetEquipment {
 			String message = operatingSystem.getDataVersion();
 			message += " Route table: " + routingTable.toString();
 			responsePacket.setText(message);
-			if(connectedEquipments != null && connectedEquipments.size() > 0){
-				for(Equipment equipment : connectedEquipments){
-					if(equipment.associatedIp.sameNetwork(packet.getDestination())){
-						equipment.receivePacket(responsePacket);
-						return;
-					}
-				}
+			if(this.equipments != null && !this.equipments.isEmpty()){
+				this.sendPacket(responsePacket);
 			}
 			defaultEquipment.receivePacket(responsePacket);
 		}
